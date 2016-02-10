5dab5f556c046c7d3113803ee7e67e46153b9996
I <daniel.sada@edgebound.net>
2016-02-10 10:19:56 -0600

    Initial Commit

---
+++ desktop/src/mx/itesm/donchito/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.donchito.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.donchito.DonChito;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DonChito(), config);
+	}
+}
---
+++ html/src/mx/itesm/donchito/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package mx.itesm.donchito.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import mx.itesm.donchito.DonChito;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DonChito();
+        }
+}
