22f0402127310efedfaddb20076636b081ab56dd
Joel Lara <joel.lq@hotmail.com>
2016-02-12 15:43:04 -0600

    Main menu initial

--- desktop/src/mx/itesm/donchito/desktop/DesktopLauncher.java
+++ desktop/src/mx/itesm/donchito/desktop/DesktopLauncher.java
@@ -6,7 +6,7 @@ import mx.itesm.donchito.DonChito;
 
 public class DesktopLauncher {
     private static final int SCREEN_WIDTH = 1280;
-    private static final int SCREEN_HEIGHT =1280;
+    private static final int SCREEN_HEIGHT =720;
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new DonChito(), config);
