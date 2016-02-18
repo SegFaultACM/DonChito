e18845129d2303cf0c50de32bc92a00a87409501
Daniel Sada <daniel.sada@edgebound.net>
2016-02-18 12:18:41 -0600

    Added constants to "AcercaDe"

--- core/src/mx/itesm/donchito/Constants.java
+++ core/src/mx/itesm/donchito/Constants.java
@@ -8,4 +8,24 @@ public class Constants {
     public static final String MENUPRINCIPAL_CARTELDONCHITO_PNG = "Imagenes/Menuprincipal/carteldonchito.png";
     public static final String MUSICA_FLAVIO_SAYS_MP3 = "Musica/FlavioSays.mp3";
     public static final String MENUPRINCIPAL_NUEVAPARTIDA_PNG = "Imagenes/Menuprincipal/nuevapartida.png";
+    public static final String PANTALLA_CONFIG_PNG = "Imagenes/PantallasConfig/Config.png";
+
+
+    // Texts
+    public static final String TITULO_ACERCA_DE =  "Primer entregable de Videojuegos.";
+    public static final String TEXTO_ACERCA_DE = "Don chito es un pueblerino ejemplar el \n" +
+            "cual ha sido designado para proteger el mineral que \n" +
+            "hace que el pueblo Laurencio destaque de los demás. \n" +
+            "Siempre busca la honestidad,amabilidad,confianza,\n " +
+            "siempre busca el bienestar del prójimo.\n" +
+            " El será quien luche contra el enemigo para salvar al pueblo.\n\n" +
+            "Daniel Sada Caraveo A01169735\n" +
+            "Joel Lara Quintana A01374649\n" +
+            "Esteban Gil Martínez A01375048\n" +
+            "Luis Manuel Alcala Alcaraz   A01372538\n" +
+            "Karla Aidee Gonz\u00e1lez Vega A01373938\n";
+
+
+
+
 }
