3ab8c98e0057d7277020179731b3bfdaea5eba89
Daniel Sada <danielsadac@gmail.com>
2016-03-11 15:12:36 -0600

    Constants

--- core/src/mx/itesm/donchito/Constants.java
+++ core/src/mx/itesm/donchito/Constants.java
@@ -6,10 +6,18 @@ public class Constants {
     public static final String MENUPRINCIPAL_CARGARPARTIDA_PNG = "Imagenes/Menuprincipal/cargarpartida.png";
     public static final String MENUPRINCIPAL_FONDO_JPG = "Imagenes/Menuprincipal/menuprincipal.jpg";
     public static final String MENUPRINCIPAL_CARTELDONCHITO_PNG = "Imagenes/Menuprincipal/carteldonchito.png";
-    public static final String MUSICA_FLAVIO_SAYS_MP3 = "Musica/FlavioSays.mp3";
+    public static final String MENUPRINCIPAL_NUEVAPARTIDAFONDO_PNG = "Imagenes/Menuprincipal/nuevapartidaluz.png";
     public static final String MENUPRINCIPAL_NUEVAPARTIDA_PNG = "Imagenes/Menuprincipal/nuevapartida.png";
     public static final String MENUPRINCIPAL_PALA_PNG = "Imagenes/Menuprincipal/pala.png";
     public static final String MENUPRINCIPAL_EXTRA_PNG = "Imagenes/Menuprincipal/Extra.png";
+
+    //Cueva
+    public static final String CUEVA_FONDO_JPG = "Imagenes/Cueva/fondoCueva.jpg";
+    public static final String CUEVA_DON_CHITO_PNG = "Imagenes/Cueva/donChito.png";
+    public static final String CUEVA_ARROW_UP = "Imagenes/Cueva/ArrowUp.png";
+    public static final String CUEVA_ARROW_DOWN = "Imagenes/Cueva/ArrowDown.png";
+    public static final String CUEVA_ARROW_LEFT = "Imagenes/Cueva/ArrowLeft.png";
+    public static final String CUEVA_ARROW_RIGHT = "Imagenes/Cueva/ArrowRight.png";
 
     //FlevorioSays
     public static final String FLEVORIO_FONDOPANTALLA_PNG = "Imagenes/Simon/fondosimon.jpg";
@@ -17,21 +25,28 @@ public class Constants {
     public static final String FLEVORIO_BOTONCENTRAL_PNG = "Imagenes/Simon/botonCentral.png";
     public static final String FLEVORIO_BOTON2_PNG = "Imagenes/Simon/nivel2.png";
     public static final String FLEVORIO_BOTON3_PNG = "Imagenes/Simon/nivel3.png";
+
+
+    public static final String FLEVORIO_BOTON_PAUSA_PNG = "Imagenes/Pausa/stop_boton.png";
+    public static final String FLEVORIO_BOTON_PLAY_PNG = "Imagenes/Pausa/play_boton.png";
+    public static final String FLEVORIO_BOTON_CONFIGURACION_PNG = "Imagenes/Pausa/pausa_herramientas.png";
+    public static final String FLEVORIO_BOTON_SALIRMENU_PNG = "Imagenes/Pausa/pausa_regresarAlMenuPrincipal.png";
+
+    public static final String FLEVORIO_MENU_PAUSA_PNG = "Imagenes/Pausa/marco_fondo.png";
+
     public static final String FLEVORIO_SONIDOBOTON_WAV = "Musica/efectoBoton.wav";
     public static final String FLEVORIO_SONIDOFAIL_WAV = "Musica/fail.wav";
     public static final String FLEVORIO_SONIDOVICTORY_WAV = "Musica/victory.wav";
     public static final String FLEVORIO_MUSICAINTRO_WAV = "Musica/flevorioIntro2.mp3";
     public static final String FLEVORIO_MUSICAFONDO_WAV = "Musica/flevorioFondo.mp3";
 
-
     //Ajustes
     public static final String AJUSTES_BOTON_PNG = "Imagenes/Pausa/pausa_herramientas.png";
-
     //Extras
     public static final String EXTRAS_FONDO_JPG = "Imagenes/Extras/plataformaExtras.jpg";
 
     //Configuracion
-    public static final String PANTALLA_CONFIG_PNG = "Imagenes/PantallasConfig/Config.png";
+    //public static final String PANTALLA_CONFIG_PNG = "Imagenes/PantallasConfig/Config.png";
 
 
     // Texts
