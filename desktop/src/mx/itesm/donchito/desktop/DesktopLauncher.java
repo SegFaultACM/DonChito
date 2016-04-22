43e5c2f071507b779e94adbfa4fb5c977f53751b
Joel Lara <joel.lq@hotmail.com>
2016-04-22 15:08:15 -0500

    Icon Launcher

--- desktop/src/mx/itesm/donchito/desktop/DesktopLauncher.java
+++ desktop/src/mx/itesm/donchito/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package mx.itesm.donchito.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import mx.itesm.donchito.DonChito;
@@ -8,9 +9,12 @@ public class DesktopLauncher {
     private static final int SCREEN_WIDTH = 1280;
     private static final int SCREEN_HEIGHT =720;
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DonChito(), config);
-		config.width = SCREEN_WIDTH;
-        config.height = SCREEN_HEIGHT;
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Don Chito";
+		cfg.addIcon("Imagenes/ic_launcher.png", Files.FileType.Internal);
+		cfg.width = SCREEN_WIDTH;
+		cfg.height = SCREEN_HEIGHT;
+		new LwjglApplication(new DonChito(), cfg);
+
 	}
 }
